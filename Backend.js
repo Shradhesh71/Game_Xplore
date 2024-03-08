87a21b5a2e86de5091fd933490fc2dde45b80275
Shradhesh_jain <shradeshjain123@gmail.com>
2024-03-08 11:29:47 +0530

    update search file

--- Backend.js
+++ Backend.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const fs = require('fs');
-const port = process.env.PORT || 8000;
+const path = require("path");
+const fs = require("fs");
+const port = process.env.PORT || 5000;
 const cookiePaser = require("cookie-parser");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const {
-    checkForAuthenticationCookie,
-  } = require("./middlewares/authentication");
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 
 // const Gameuser = require("../models/user");
 
 const userRoute = require("./routes/user");
 const GoogleRoute = require("./routes/googleAuth");
-const searchRoute= require("./routes/search");
-const friendchat= require("./routes/friendchat");
-
+const searchRoute = require("./routes/search");
+const friendchat = require("./routes/friendchat");
 
 const session = require("express-session");
-const passport = require('passport');
+const passport = require("passport");
 
 // const {connectmongoose, Gameuser} = require("./database.js");
 // const {intinalizingPassport} = require("./passport.js");
@@ -31,36 +30,36 @@ const axios = require("axios");
 // app.use(bodyparser.urlencoded({extended: true}));
 // const _ = require("lodash");
 // const {storage,uploadfile} = require("./multer.js");
-const multer = require('multer');
+const multer = require("multer");
 // const mongostore = require("connect-mongo");
 
+mongoose
+  .connect(
+    "mongodb+srv://shradesh71:newone71@cluster0.4tegtua.mongodb.net/gameusers?retryWrites=true"
+  )
+  .then(() => {
+    console.warn("connect...");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-mongoose.connect('mongodb+srv://shradesh71:newone71@cluster0.4tegtua.mongodb.net/gameusers?retryWrites=true')
-    .then(()=>{
-        console.warn("connect...");
-    })
-    .catch((error)=>{
-        console.log(error);
-    });
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // set the veiw directory
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views')); // set the veiw directory
-    
+app.use("/static", express.static("static"));
+app.use("/veiws", express.static("veiws")); // for serving static files
 
-app.use('/static',express.static('static'));
-app.use('/veiws',express.static('veiws'));// for serving static files
-
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookiePaser());
 app.use(checkForAuthenticationCookie("token"));
-
 
 // app.use(session({   //Save login session
 //   secret: "ShradheshJain!!!",
 //   resave: false,
 //   saveUninitialized: true,
 //   store: mongostore.create({ mongoUrl:"mongodb+srv://shradesh71:newone71@cluster0.4tegtua.mongodb.net/gameusers?retryWrites=true",collectionName: "sessions" }),
-//   cookie:{ 
+//   cookie:{
 //     maxAge: 1000*60*60 // 1 hours
 //   }
 // }));
@@ -73,22 +72,54 @@ app.use(checkForAuthenticationCookie("token"));
 
 // intinalizingPassport(passport);
 
-const countrycodes =[];
+const countrycodes = [];
 
 // const uploadDirectory = './uploads';
 // if (!fs.existsSync(uploadDirectory)) {
 //   fs.mkdirSync(uploadDirectory);
 // };
+const headers = {
+  "Client-ID": "hp48e43i91iisl72by8u6zdbge0vnc",
+  Authorization: `Bearer jz59r1zny81xu2ff78bco9binsayb9`,
+  Accept: "application/json",
+};
+const gameUrl = `https://api.igdb.com/v4/games?search=valorant&limit=5&fields=videos.name,videos.video_id,screenshots.image_id,screenshots.url,cover.image_id,cover.animated,websites.url,name,rating,cover.url,url`;
 
+app.get("/", async (req, res) => {
+  await axios
+    .get(gameUrl, {
+      headers,
+    })
+    .then((res) => {
+      //   console.log(res.data);   1.websites[0].url
+      gameData = res.data;
+      console.log("----------------------------------------------------");
+      console.log(gameData[0].videos[0].video_id);
+      console.log(gameData[1].videos[0].video_id);//  1.videos[0].video_id
+      console.log(gameData[2].videos[0].video_id);
+      console.log(gameData[1].websites[0].url);
+    })
+    .catch((err) => console.error(err));
 
-app.get('/',async (req, res) =>{
-  res.render("main_F",{
+  res.render("main_F", {
     user: req.user,
+    gameName0: gameData[0].name,
+    gameName1: gameData[1].name,
+    gameName2: gameData[2].name,
+    // gameName3: gameData[3].name,
+    gameweb0:gameData[0].websites[0].url,
+    gameweb1:gameData[1].websites[0].url,
+    gameweb2:gameData[2].websites[0].url,
+    // gameweb3:gameData[3].websites[0].url,
+    gameyou0:gameData[0].videos[0].video_id,
+    gameyou1:gameData[1].videos[0].video_id,
+    gameyou2:gameData[2].videos[0].video_id,
+    // gameyou3:gameData[3].videos[0].video_id,
   });
-    console.log("Sent: Main_F Page");
+  console.log("Sent: Main_F Page");
 });
 
-app.get("/error",async (req, res) =>{
+app.get("/error", async (req, res) => {
   res.render("404");
 });
 
@@ -107,8 +138,6 @@ app.use("/auth", GoogleRoute);
 app.use("/search", searchRoute);
 app.use("/friend", friendchat);
 
-
-
 // app.get('/login',(req, res) =>{
 //     res.status(200).render("login")
 //     console.log("Sent: Login page");
@@ -120,14 +149,14 @@ app.use("/friend", friendchat);
 // });
 
 // app.get('/register_last',(req,res) =>{
-//   if(countrycodes.length==0){
-//     res.redirect("/register");
-//     console.log("Sent: Register_last to Register Page");
-//   }else{
-//     res.status(200).render("register2");
-//     console.log("Sent: Register_last_Page");
-//   }
-// });
+// //   if(countrycodes.length==0){
+// //     res.redirect("/register");
+// //     console.log("Sent: Register_last to Register Page");
+// //   }else{
+// //     res.status(200).render("register2");
+// //     console.log("Sent: Register_last_Page");
+// //   }
+// // });
 
 // app.get("/avatar/:gxid",(req, res) =>{
 //   const gxid = _.capitalize(req.params.gxid);
@@ -135,115 +164,119 @@ app.use("/friend", friendchat);
 //   res.render("avatar",{"gxid": gxid});
 // });
 
-app.get("/studios",(req,res) =>{
+app.get("/studios", (req, res) => {
   res.render("studios");
   console.log("Sent: Studios_Page");
 });
 
-app.get("/useralready",(req,res) =>{
+app.get("/useralready", (req, res) => {
   res.render("userAlready");
 });
 
 // app.get("/Game",(req,res) =>{
 //   res.render("about_game.ejs");
 // });
-const apiKey = '227ead613574b24dba4bbad59eb633dabdc43a2b';
+const apiKey = "227ead613574b24dba4bbad59eb633dabdc43a2b";
 
-app.post("/game",async (req,res) =>{
-  console.log(req.body);
-  try {
-    const gameId = req.body.gamen;
-    const response = await axios.get(
-      `https://www.giantbomb.com/api/game/${gameId}/?api_key=${apiKey}&format=json`
-    );
-    // console.log(response);
-    const gameData = response.data.results;
-    // const params = {gametitle:gameData.name,
-    //                 gamemainimage:gameData.image.medium_url,
-    //                 gamerelease:gameData.original_release_date,
-    //                 gamedeck:gameData.deck,
-    //                 gamedisp:gameData.description,
-    //                 gamesimilar:gameData.similar_games[0].name,
-    //                 gamesemiurl:gameData.similar_games[0].site_detail_url
-    //               }
-    // res.json(gameInfo);
-  res.render("about_game",{
-    user: req.user,
-    gametitle:gameData.name,
-    gamemainimage:gameData.image.medium_url,
-    gamerelease:gameData.original_release_date,
-    gamedeck:gameData.deck,
-    gamedisp:gameData.description,
-    gamesimilar:gameData.similar_games[0].name,
-    gamesemiurl:gameData.similar_games[0].site_detail_url
-  });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.post('/register',  (req, res) =>{
-    countrycodes.push(req.body.country);
-    console.log('Request country Code: ',req.body.country);
-    res.status(200).redirect("/user/signup");
-    console.log("Sent: Register_1 Page Post Request");
-
-});
-
-// app.get("/logout",(req, res) => {
-//   req.logout();
-//   res.redirect("/"); 
+// app.post("/game", async (req, res) => {
+//   console.log(req.body);
+//   try {
+//     const gameId = req.body.gamen;
+//     const response = await axios.get(
+//       `https://www.giantbomb.com/api/game/${gameId}/?api_key=${apiKey}&format=json`
+//     );
+//     // console.log(response);
+//     const gameData = response.data.results;
+//     // const params = {gametitle:gameData.name,
+//     //                 gamemainimage:gameData.image.medium_url,
+//     //                 gamerelease:gameData.original_release_date,
+//     //                 gamedeck:gameData.deck,
+//     //                 gamedisp:gameData.description,
+//     //                 gamesimilar:gameData.similar_games[0].name,
+//     //                 gamesemiurl:gameData.similar_games[0].site_detail_url
+//     //               }
+//     // res.json(gameInfo);
+//     res.render("about_game", {
+//       user: req.user,
+//       gametitle: gameData.name,
+//       gamemainimage: gameData.image.medium_url,
+//       gamerelease: gameData.original_release_date,
+//       gamedeck: gameData.deck,
+//       gamedisp: gameData.description,
+//       gamesimilar: gameData.similar_games[0].name,
+//       gamesemiurl: gameData.similar_games[0].site_detail_url,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
 // });
 
-// app.post('/register_last',async (req, res) =>{
-//   // console.log(req.body.email);
-//     // const newUser = await Gameuser.findOne({username:req.body.username});
+app.post("/register", (req, res) => {
+  countrycodes.push(req.body.country);
+  console.log("Request country Code: ", req.body.country);
+  res.status(200).redirect("/user/signup");
+  console.log("Sent: Register_1 Page Post Request");
+});
 
-//     // if(newUser) return res.status(400).redirect("/useralready");
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
-//     //  const newUser = await Gameuser.create(req.body);
-//     const user = new Gameuser({
-//       email: req.body.username,
-//       password: req.body.password,
-//       gxidnames: req.body.name
-//     });
-//     await user.save()
-//     .then(user=>{
-//       console.log(user);
-//     }).catch(err=>{
-//       console.log(err);
-//     });
-//     res.status(201).redirect("/home");
-//     console.log("Sent: Register_last Page Post Request");
-// })
+app.post("/register_last", async (req, res) => {
+  // console.log(req.body.email);
+  // const newUser = await Gameuser.findOne({username:req.body.username});
+
+  // if(newUser) return res.status(400).redirect("/useralready");
+
+  //  const newUser = await Gameuser.create(req.body);
+  const user = new Gameuser({
+    email: req.body.username,
+    password: req.body.password,
+    gxidnames: req.body.name,
+  });
+  await user
+    .save()
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.status(201).redirect("/home");
+  console.log("Sent: Register_last Page Post Request");
+});
 
 // app.post('/login', passport.authenticate("local", { failureRedirect: '/login' }), (req, res) => {
 //   res.redirect('/');
 // });
 
 const storage = multer.diskStorage({
-  destination:  (req, file, callback) => {
-      fs.opendir('./uploads', (err) => {  
-          if(err) {
-              console.log(err.stack)
-          } else {
-              callback(null, './uploads');
-          }
-          console.log('Finished uploading!');
-      })
+  destination: (req, file, callback) => {
+    fs.opendir("./uploads", (err) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        callback(null, "./uploads");
+      }
+      console.log("Finished uploading!");
+    });
   },
-  filename: (req, file, callback) =>{
-      callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // for file extension
-  }
+  filename: (req, file, callback) => {
+    callback(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    ); // for file extension
+  },
 });
-app.post("/avatar",(req,res)=>{
+app.post("/avatar", (req, res) => {
   // const profilePicture = req.body.avatar;
   // console.log(profilePicture);
-//   const profile = new Item({
-//     profilePicture: profilePicture
-// });
-console.log(req.body);
-// res.send(req.body);
+  //   const profile = new Item({
+  //     profilePicture: profilePicture
+  // });
+  console.log(req.body);
+  // res.send(req.body);
   // const user = await Gameuser.findOne({username:req.body.username})
   // .then((user)=>{
   //   user.profiles.push(profile);
@@ -253,7 +286,7 @@ console.log(req.body);
   // .catch((err)=>{
   //   res.status(500).send('Error uploading profile picture.');
   // })
-  
+
   // try {
   //   ()=>{
 
@@ -263,25 +296,31 @@ console.log(req.body);
   // } catch (error) {
   //   res.status(500).send('Error uploading profile picture.');
   // }
-  var upload = multer({ storage : storage,
-    fileFilter: (req, file, callback) =>{    // this function is used to filter upload by extension
-        var ext = path.extname(file.originalname+".jpg");
-        if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-            return callback(new Error('Only images are allowed'))
-        }
-        callback(null, true)
-    }
-}).single('userFile');
-upload(req,res,(err) =>{
-    if(err) {
-        return res.end("Error uploading file.");
+  var upload = multer({
+    storage: storage,
+    fileFilter: (req, file, callback) => {
+      // this function is used to filter upload by extension
+      var ext = path.extname(file.originalname + ".jpg");
+      if (
+        ext !== ".png" &&
+        ext !== ".jpg" &&
+        ext !== ".gif" &&
+        ext !== ".jpeg"
+      ) {
+        return callback(new Error("Only images are allowed"));
+      }
+      callback(null, true);
+    },
+  }).single("userFile");
+  upload(req, res, (err) => {
+    if (err) {
+      return res.end("Error uploading file.");
     }
     // res.end("File is uploaded");
     res.redirect("/");
+  });
 });
 
-});
-
-app.listen(port,()=>{
-    console.log(`the application started successfully on port ${port}`);
+app.listen(port, () => {
+  console.log(`the application started successfully on port ${port}`);
 });
--- routes/search.js
+++ routes/search.js
@@ -1,35 +1,46 @@
 const { Router } = require("express");
-const axios = require("axios");
-const Gameuser = require("../models/user");
-
 const router = Router();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
 
-const apiKey = '227ead613574b24dba4bbad59eb633dabdc43a2b';
+router.use(bodyParser.json());
+router.use(cors());
 
-router.post("/game",async (req,res)=>{
-    const {gamen} = req.body;
+router.post("/game", async (req, res) => {
+  const { gamen } = req.body;
+  const headers = {
+    "Client-ID": "hp48e43i91iisl72by8u6zdbge0vnc",
+    Authorization: `Bearer jz59r1zny81xu2ff78bco9binsayb9`,
+    Accept: "application/json",
+  };
+  let gameData;
 
-    try {
-        const gameId = gamen;
-        const response = await axios.get(
-        `https://www.giantbomb.com/api/game/${gameId}/?api_key=${apiKey}&format=json`
-        );
-        const gameData = response.data.results;
-        return res.render("about_game",{
-            user: req.user,
-            gametitle:gameData.name,
-            gamemainimage:gameData.image.medium_url,
-            gamerelease:gameData.original_release_date,
-            gamedeck:gameData.deck,
-            gamedisp:gameData.description,
-            gamesimilar:gameData.similar_games[0].name,
-            gamesemiurl:gameData.similar_games[0].site_detail_url
-          });
-    } catch (error) {
-        console.log("error: ",error);
-        return res.redirect("/error");
-    }
+  const gameUrl = `https://api.igdb.com/v4/games?search=${gamen}&limit=1&fields=videos.name,videos.video_id,videos.checksum,screenshots.image_id,screenshots.url,themes.name,themes.url,cover.image_id,cover.animated,involved_companies.company.name,similar_games.name,tags,release_dates.date,genres.name,release_dates.region,websites.url,name,rating,cover.url,platforms.name,url,summary,storyline`;
+  await axios
+    .get(gameUrl, {
+      headers,
+    })  
+    .then((res) => {
+    //   console.log(res.data);
+      gameData = res.data;
+      console.log("----------------------------------------------------");
+      console.log(gameData[0].name);
+    })
+    .catch((err) => console.error(err));
+    console.log(gameData[0].similar_games);
+  return res.render("about_game", {
+    user: req.user,
+    gametitle: gameData[0].name,
+    gamemainimage: gameData[0].cover.url,
+    gamerelease: gameData[0].release_dates[0].date,
+    gamerate: gameData[0].rating,
+    gamedisp: gameData[0].summary,
+    similar_games: gameData[0].similar_games,
+    theme:gameData[0].themes,
+    download: gameData[0].websites,
+    company: gameData[0].involved_companies
+  });
 });
-
 
 module.exports = router;
